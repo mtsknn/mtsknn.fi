a7c1a37cf8a15b2704d35d83e028b71c50c1a9d6
Matias Kinnunen <github@mtsknn.fi>
2020-09-25 14:52:45 +0300

    Add Tailwind and basic styling to layout

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  future: {
+    removeDeprecatedGapUtilities: true,
+    purgeLayersByDefault: true,
+  },
+  purge: ['content/_includes/**/*.pug'],
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+}
