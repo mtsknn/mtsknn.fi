8a56dd3c81238515027fb6e91e0d02f5961cba22
Matias Kinnunen <github@mtsknn.fi>
2020-09-27 11:18:47 +0300

    Add link styles

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,13 @@
+const md = require('./data/md')
+
 module.exports = (config) => {
   config.addCollection('blog_posts', (collection) =>
     [...collection.getFilteredByGlob('./content/blog/**/*.md')].reverse()
   )
 
   config.addPassthroughCopy({ './assets/favicon/': '/' })
+
+  config.setLibrary('md', md)
 
   return {
     dir: {
--- data/md.js
+++ data/md.js
@@ -1,7 +1,11 @@
 const markdownIt = require('markdown-it')({ html: true })
+markdownIt.use(require('markdown-it-link-attributes'), {
+  attrs: {
+    class: 'link',
+  },
+})
 
 // Since Pug filters don't support dynamic data (e.g. `:md= post.data.intro`),
-// let's instead create a global `md.render()` function
-module.exports = {
-  render: (text) => markdownIt.render(text),
-}
+// let's instead export the whole `markdown-it` parser. This is also used in the
+// 11ty config
+module.exports = markdownIt
