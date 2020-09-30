375f530ede7e01a4ea322da1d164fd25be6fe27c
Matias Kinnunen <github@mtsknn.fi>
2020-09-30 05:54:45 +0300

    Update Browsersync options

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,5 @@
-const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight")
+const fs = require('fs')
+const syntaxHighlight = require('@11ty/eleventy-plugin-syntaxhighlight')
 
 const md = require('./data/md')
 
@@ -10,6 +11,30 @@ module.exports = (config) => {
   config.addPassthroughCopy({ './assets/favicon/': '/' })
 
   config.addPlugin(syntaxHighlight)
+
+  config.setBrowserSyncConfig({
+    callbacks: {
+      // This nifty code was copied from here:
+      // https://github.com/11ty/eleventy-base-blog/blob/v5.0.2/.eleventy.js#L56-L64
+      ready(err, browserSync) {
+        const notFoundContent = fs.readFileSync('./_site/404.html')
+
+        // Provides the 404 content without redirect
+        browserSync.addMiddleware('*', (req, res) => {
+          res.write(notFoundContent)
+          res.end()
+        })
+      },
+    },
+    files: './_site/main.css',
+    ghostMode: false,
+    open: true,
+    ui: false,
+
+    /* Uncomment this to create a public URL (`something.loca.lt`); useful when
+     * testing with mobile devices */
+    // tunnel: true,
+  })
 
   // "This will likely become the default in an upcoming major version."
   // See https://www.11ty.dev/docs/data-deep-merge/
