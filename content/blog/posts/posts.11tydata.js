7afe6fdf042c87a18f4d6d08f4832a827538b1a8
Matias Kinnunen <github@mtsknn.fi>
2020-10-04 17:51:02 +0300

    Refactor the structure of `content/blog/` folder
    This makes the next change easier: draft and scheduled posts

--- content/blog/posts/posts.11tydata.js
+++ content/blog/posts/posts.11tydata.js
@@ -2,5 +2,9 @@ module.exports = {
   // Default to an empty string or `md.render()` will break in many places
   intro: '',
 
+  eleventyComputed: {
+    permalink: (data) =>
+      data.page.filePathStem.replace('/blog/posts/', '/blog/') + '/',
+  },
   layout: 'post.pug',
 }
