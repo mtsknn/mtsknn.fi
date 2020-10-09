cc03cb274abd6a2b3a44ba2cfe5545bdcc265234
Matias Kinnunen <github@mtsknn.fi>
2020-10-09 10:40:14 +0300

    Fix url slug of the "C#" tag

--- data/slugify.js
+++ data/slugify.js
@@ -2,5 +2,10 @@ const slugify = require('slugify')
 
 // "If you return a `function`, we'll use the return value from that function."
 // Source: https://www.11ty.dev/docs/data-js/
-module.exports = () => (text = '') =>
-  slugify(text, { lower: true, strict: true })
+module.exports = () => (text = '') => {
+  // This works for the "C#" tag but not when a longer text contains "C#". Might
+  // want to do something about this someday
+  if (text.toLowerCase() === 'c#') return 'csharp'
+
+  return slugify(text, { lower: true, strict: true })
+}
