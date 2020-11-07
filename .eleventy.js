7a1eaf87a421507414ccaf3fcf2c81a9f2702c37
Matias Kinnunen <github@mtsknn.fi>
2020-11-07 19:14:01 +0200

    Add temporary solution for copying blog post images

--- .eleventy.js
+++ .eleventy.js
@@ -23,6 +23,24 @@ module.exports = (config) => {
 
   config.addPassthroughCopy({ './assets/favicon/': '/' })
 
+  /* TODO: This works only for this single blog post.
+   * Relevant issue: <https://github.com/11ty/eleventy/issues/1140>.
+   * So there are basically two options:
+   * • Create a JS script which copies images to the correct folders
+   *   • Unnecessarily complex solution?
+   * • Adapt to Eleventy's default behavior as much as possible, i.e.:
+   *   • Modify the folder structure to remove the need for custom permalinks
+   *     • Move files in `content/blog/feeds/` to e.g. `content/blog-feeds/`
+   *     • Move files in `content/blog/tags/` to e.g. `content/blog-tags/`
+   *     • Copy the contents of `_template.md` and `posts.11tydata.js` to the readme
+   *       because the files will now be in the private Git submodule
+   *   • Use the standard `addPassthroughCopy` for all images
+   * The second option seems better
+   */
+  config.addPassthroughCopy({
+    './content/**/*.png': '/blog/episerver-how-to-change-contents-modified-date-programmatically/'
+  })
+
   config.addPlugin(pluginSyntaxHighlight, { alwaysWrapLineHighlights: true })
 
   config.addTransform('heading-anchor-links', transformHeadingAnchorLinks)
