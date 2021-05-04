a9a275291ef4ce5786a8bff14e9fc4d565d2cdb2
Matias Kinnunen <github@mtsknn.fi>
2021-05-04 08:15:44 +0300

    Show intro texts of weekly log entries

--- _sample-content/weekly-log/weekly-log.11tydata.js
+++ _sample-content/weekly-log/weekly-log.11tydata.js
@@ -1,4 +1,7 @@
 module.exports = {
-  intro: 'Default intro text',
+  // Without a default value, `md.render(intro)` would throw an error.
+  // Besides, a heads-up text like this is more helpful.
+  intro: '⚠ *Missing intro text*',
+
   layout: 'weekly-log-entry',
 }
