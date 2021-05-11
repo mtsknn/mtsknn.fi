61c421618cdfbfad139ef2e9e0bd102d35c87916
Matias Kinnunen <github@mtsknn.fi>
2021-05-11 08:09:11 +0300

    Install `markdown-it-deflist` for definition lists
    Needed it here: https://mtsknn.fi/blog/rfc-2119-in-a-nutshell/

--- data/md.js
+++ data/md.js
@@ -1,6 +1,7 @@
 const markdownIt = require('markdown-it')
 const markdownItAnchor = require('markdown-it-anchor')
 const markdownItAttrs = require('markdown-it-attrs')
+const markdownItDeflist = require('markdown-it-deflist')
 const markdownItFootnote = require('markdown-it-footnote')
 const markdownItLinkAttributes = require('markdown-it-link-attributes')
 
@@ -20,6 +21,7 @@ const md = markdownIt({ html: true })
     // `heading-anchor-links` transform
   })
   .use(markdownItAttrs)
+  .use(markdownItDeflist)
   .use(markdownItFootnote)
   .use(markdownItLinkAttributes, [
     {
