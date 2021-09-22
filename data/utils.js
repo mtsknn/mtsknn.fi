d22196c81844f8152848b365bbd67706c8f148a2
Matias Kinnunen <github@mtsknn.fi>
2021-09-22 20:50:48 +0300

    Improve usage of Plausible Analytics (#20)

--- data/utils.js
+++ data/utils.js
@@ -36,6 +36,19 @@ const utils = {
         return found
       }),
   isDraft: (data) => data.page.filePathStem.includes('/drafts/'),
+
+  /**
+   * Checks whether the current Netlify deploy context is `production`.
+   *
+   * Other possible contexts are `deploy-preview` and `branch-deploy`,
+   * so returns `false` in deploy previews ("PR previews") and branch deploys.
+   *
+   * Returns `false` in non-Netlify environments, e.g. localhost.
+   *
+   * @see <https://docs.netlify.com/configure-builds/environment-variables/#build-metadata>
+   */
+  isNetlifyProductionContext: () => process.env.CONTEXT === 'production',
+
   isProductionEnv: process.env.NODE_ENV === 'production',
   isScheduled: (data) => data.date >= now,
 }
