1718bd78ea154d3649e944ae15640399612b0852
Matias Kinnunen <github@mtsknn.fi>
2021-10-04 19:42:40 +0300

    Move content from robots.txt layout to content file
    D'oh!

--- _sample-content/robots.md
+++ _sample-content/robots.md
@@ -3,3 +3,8 @@ layout: Robots
 eleventyExcludeFromCollections: true
 permalink: robots.txt
 ---
+
+User-agent: *
+Allow: /
+
+Sitemap: {{ site.url }}{{ site.sitemapUrl }}
--- layouts/Robots.11ty.js
+++ layouts/Robots.11ty.js
@@ -1,10 +1,1 @@
-const dedent = require('dedent')
-
-const site = require('../data/site')
-
-module.exports = dedent`
-  User-agent: *
-  Allow: /
-
-  Sitemap: ${site.url}${site.sitemapUrl}
-`
+module.exports = ({ content }) => content.trimStart()
