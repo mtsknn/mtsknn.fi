0cfcca6d49bcbd7b7b8c49392e751b8ddb169a09
Matias Kinnunen <github@mtsknn.fi>
2021-10-16 22:01:30 +0300

    Improve footnotes (#28)

--- utils/md.js
+++ utils/md.js
@@ -54,7 +54,80 @@ const md = markdownIt({
     },
   ])
 
-// Disable the default `<hr>` and CSS classes
-md.renderer.rules.footnote_block_open = () => '<section><ol>'
+// Original:
+// https://github.com/markdown-it/markdown-it-footnote/blob/3.0.3/index.js#L29-L39
+md.renderer.rules.footnote_ref = (tokens, idx, options, env, slf) => {
+  const id = slf.rules.footnote_anchor_name(tokens, idx, options, env, slf)
+  const caption = slf.rules.footnote_caption(tokens, idx, options, env, slf)
+  let refid = id
+
+  if (tokens[idx].meta.subId > 0) {
+    refid += ':' + tokens[idx].meta.subId
+  }
+
+  return render(html`
+    <sup>
+      <a
+        aria-label=${`footnote ${id}`}
+        class="link"
+        href=${`#fn-${id}`}
+        id=${`fnref-${refid}`}
+      >
+        ${caption}
+      </a>
+    </sup>
+  `)
+}
+
+// Original:
+// https://github.com/markdown-it/markdown-it-footnote/blob/3.0.3/index.js#L41-L45
+md.renderer.rules.footnote_block_open = () => `
+  <hr aria-hidden="true">
+  <section aria-label="Footnotes">
+    <h2 class="!text-base !text-gray-700 tracking-widest uppercase xl:!text-lg">
+      Footnotes
+    </h2>
+    <ol>
+`
+
+// Original:
+// https://github.com/markdown-it/markdown-it-footnote/blob/3.0.3/index.js#L51-L59
+md.renderer.rules.footnote_open = (tokens, idx, options, env, slf) => {
+  let id = slf.rules.footnote_anchor_name(tokens, idx, options, env, slf)
+
+  if (tokens[idx].meta.subId > 0) {
+    id += ':' + tokens[idx].meta.subId
+  }
+
+  return `<li id="${`fn-${id}`}">`
+}
+
+// Original:
+// https://github.com/markdown-it/markdown-it-footnote/blob/3.0.3/index.js#L65-L74
+md.renderer.rules.footnote_anchor = (tokens, idx, options, env, slf) => {
+  let id = slf.rules.footnote_anchor_name(tokens, idx, options, env, slf)
+
+  if (tokens[idx].meta.subId > 0) {
+    id += ':' + tokens[idx].meta.subId
+  }
+
+  return render(
+    html`
+      ${' '}
+      <a
+        aria-label="Back to reference"
+        class="font-mono link"
+        href=${`#fnref-${id}`}
+      >
+        <!--
+          Without the 'font-mono' class
+          this would look like an emoji instead of a Unicode symbol
+          at least in Chrome on Windows and Chrome on Android
+        -->
+        ↩
+      </a>
+    `
+  )
+}
 
 module.exports = md
