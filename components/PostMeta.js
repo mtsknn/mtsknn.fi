5e95fe757858b8fd3b17555df07582ad37bfdfd7
Matias Kinnunen <github@mtsknn.fi>
2021-10-28 08:56:00 +0300

    Use `Intl.ListFormat` to format lists of tag links (#29)

--- components/PostMeta.js
+++ components/PostMeta.js
@@ -3,6 +3,9 @@ const { html } = require('htm/preact')
 const datetime = require('../utils/datetime')
 const slugify = require('../utils/slugify')
 
+// Using `en-GB` because it doesn't use Oxford commas (`en-US` does)
+const listFormatter = new Intl.ListFormat('en-GB', { type: 'conjunction' })
+
 module.exports = ({ date, updated, tags = [] }) => html`
   <p class="!mb-0">
     Published on${' '}
@@ -21,18 +24,17 @@ module.exports = ({ date, updated, tags = [] }) => html`
       ${' '}
       <span class="inline-block">
         in${' '}
-        ${tags.map(
-          (tag, i) => html`
-            <a
-              class="link whitespace-no-wrap"
-              href=${`/blog/tags/${slugify(tag)}/`}
-            >
-              ${tag}
-            </a>
-            ${i < tags.length - 2 && ', '}
-            <!-- -->
-            ${i === tags.length - 2 && ' and '}
-          `
+        ${listFormatter.formatToParts(tags).map(({ type, value }) =>
+          type === 'literal'
+            ? value // ', ' or ' and '
+            : html`
+                <a
+                  class="link whitespace-no-wrap"
+                  href=${`/blog/tags/${slugify(value)}/`}
+                >
+                  ${value}
+                </a>
+              `
         )}
       </span>
     `}
