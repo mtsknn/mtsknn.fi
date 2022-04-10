cfac24a12e69f48fd56c0e76890c86bb5c297b02
Matias Kinnunen <github@mtsknn.fi>
2022-04-10 06:13:14 +0300

    Add "Next/Previous blog post" links

--- layouts/BlogPost.11ty.js
+++ layouts/BlogPost.11ty.js
@@ -6,7 +6,19 @@ const PostMeta = require('../components/PostMeta')
 const TableOfContents = require('../components/TableOfContents')
 
 module.exports = (data) => {
-  const { content, intro, page, tags, title, updated } = data
+  const {
+    collections: { blogPosts },
+    content,
+    intro,
+    page,
+    tags,
+    title,
+    updated,
+  } = data
+
+  const currentPostIndex = blogPosts.findIndex(({ url }) => url === page.url)
+  const nextPost = blogPosts[currentPostIndex - 1]
+  const previousPost = blogPosts[currentPostIndex + 1]
 
   return html`
     <${Base} ...${data}>
@@ -27,6 +39,33 @@ module.exports = (data) => {
         <${TableOfContents} markdown=${content} />
         <${Markdown}>${content}<//>
       </article>
+
+      <aside class="prose xl:prose-lg">
+        <hr aria-hidden="true" class="!mt-12" />
+
+        <ul>
+          ${nextPost &&
+          html`
+            <li>
+              <em>Next blog post:</em>
+              ${' '}
+              <a class="link" href=${nextPost.url}>
+                <${Markdown} inline>${nextPost.data.title}<//>
+              </a>
+            </li>
+          `}
+          ${previousPost &&
+          html`
+            <li>
+              <em>Previous blog post:</em>
+              ${' '}
+              <a class="link" href=${previousPost.url}>
+                <${Markdown} inline>${previousPost.data.title}<//>
+              </a>
+            </li>
+          `}
+        </ul>
+      </aside>
     <//>
   `
 }
