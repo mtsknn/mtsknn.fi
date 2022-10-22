baf4a8ad2f30c064111341bf9081b561c8411795
Matias Kinnunen <github@mtsknn.fi>
2022-10-22 05:31:00 +0300

    Update `slugify` to replace en dashes with hyphens

--- utils/slugify.js
+++ utils/slugify.js
@@ -3,6 +3,7 @@ const slugify = require('slugify')
 module.exports = (text = '') => {
   return [text]
     .map((x) => x.replace(/\//g, '-'))
+    .map((x) => x.replace(/–/g, '-')) // en dash
     .map((x) => x.replace(/C#/g, 'csharp'))
     .map((x) => x.replace(/\.NET/g, 'dotnet'))
     .map((x) => slugify(x, { lower: true, strict: true }))[0]
