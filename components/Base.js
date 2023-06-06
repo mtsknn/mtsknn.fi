9fd9fb8832143184130046a895836a4030bc9eff
Matias Kinnunen <github@mtsknn.fi>
2023-06-06 13:38:25 +0300

    Update copyright year

--- components/Base.js
+++ components/Base.js
@@ -206,7 +206,7 @@ function Footer() {
       )}
     >
       <div class="text-gray-500 text-left">
-        ${entity.copy} ${site.title} 2020${entity.ndash}2022.
+        ${entity.copy} ${site.title} 2020${entity.ndash}2023.
       </div>
       <a aria-label="Scroll to top" class="ml-4 hover:opacity-75" href="#top">
         <${ArrowCircleUpIcon}
